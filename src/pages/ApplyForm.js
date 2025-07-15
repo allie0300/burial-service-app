452399c204931108eaf63f32398bfe5e2bd9349e
M A Daniels <daniels@Ms-MacBook-Pro.local>
2025-07-15 22:51:03 +0200

    Remove 'Other' option from gender dropdown

--- src/pages/ApplyForm.js
+++ src/pages/ApplyForm.js
@@ -484,7 +484,7 @@ function ApplyForm() {
                           <option value="">Select gender</option>
                           <option value="male">Male</option>
                           <option value="female">Female</option>
-                          <option value="other">Other</option>
+                        
                         </select>
                       </div>
                     </div>
